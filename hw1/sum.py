a51e97a62e2fd686a7ae7bc47eb81e39d5df6e21
TranAnh1802 <71312552+TranAnh1802@users.noreply.github.com>
2020-10-20 22:24:19 +0700

    Delete sum.py

--- hw1/sum.py
+++
@@ -1,7 +0,0 @@
-
-print("enter a series of digit with nothing separate: ")
-series = list(map(int, input()))
-sum = 0
-for i in series:
-    sum += i
- print("the sum of the digits in: ", series, "is", sum)
