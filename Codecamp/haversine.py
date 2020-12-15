3f09e1b61df9b8e68dae65fd9430196e563e3f69
TranAnh1802 <71312552+TranAnh1802@users.noreply.github.com>
2020-12-16 01:58:01 +0700

    Add files via upload

---
+++ Codecamp/haversine.py
@@ -0,0 +1,30 @@
+import math
+
+
+def haversine(lat1, lon1, lat2, lon2):
+    # distance between latitudes
+    # and longitudes
+    dLat = (lat2 - lat1) * math.pi / 180.0
+    dLon = (lon2 - lon1) * math.pi / 180.0
+
+    # convert to radians
+    lat1 = (lat1) * math.pi / 180.0
+    lat2 = (lat2) * math.pi / 180.0
+
+    # apply formulae
+    a = (pow(math.sin(dLat / 2), 2) +
+         pow(math.sin(dLon / 2), 2) *
+         math.cos(lat1) * math.cos(lat2));
+    rad = 6371
+    c = 2 * math.asin(math.sqrt(a))
+    return rad * c
+
+
+# Driver code
+if __name__ == "__main__":
+    lat1 = float(input("enter latitude of point A: "))
+    lon1 = float(input("enter longtitude of point A: "))
+    lat2 = float(input("enter latitude of point B: "))
+    lon2 = float(input("enter longtitude of point B: "))
+
+    print("distance between point A and Point B : ", round(haversine(lat1, lon1, lat2, lon2),2) ,"K.M.")
